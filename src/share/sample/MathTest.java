69fea129608358cfd1ff712753445c9a7f81a7f1
Artem Melentyev <amelentev@gmail.com>
2012-04-18 14:57:56 +0600

    add math oo test

---
+++ src/share/sample/MathTest.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+
+public class MathTest {
+    public static void main(String[] args) {
+        BigInteger a = BigInteger.ONE,
+                b = BigInteger.TEN,
+                c = a + b * b + b/b;
+        System.out.println(c.equals(BigInteger.valueOf(102)));
+    }
+}
--- src/share/test/OOTest.java
+++ src/share/test/OOTest.java
@@ -2,6 +2,7 @@ import com.sun.tools.javac.Main;
 
 public class OOTest {
     public static void main(String[] args) throws Exception {
+        Main.compile(new String[]{"src/share/sample/MathTest.java"});
         Main.compile(new String[]{"src/share/sample/MapIndexTest.java"});
         Main.compile(new String[]{"src/share/sample/ListIndexGetTest.java"});
         Main.compile(new String[]{"src/share/sample/ListIndexSetTest.java"});
